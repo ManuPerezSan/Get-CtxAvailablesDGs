19b5e92da5bb46c0099183423d8c415991f0e8a4
ManuPerezSan <beckem61@hotmail.com>
2020-05-25 22:01:41 +0200

    Last version of Get-CtxAvailablesDGs

---
+++ Get-CtxAvailablesDGs.ps1
@@ -0,0 +1,90 @@
+﻿<#
+.SYNOPSIS
+    Connect to remote XenDesktop farm (CVAD) to obtain data about availability of Delivery groups.
+
+.PARAMETER DdcServers
+    List of one Delivery Controller per farm.
+
+.PARAMETER Credential
+    Credentials to connect to remote server and XenDesktop farm (Read-Only)
+
+.RETURN
+    Return data in json format (data can be imported in influxDB)
+    
+.AUTHOR
+    Manuel Pérez
+
+.PROJECTURI
+    https://github.com/ManuPerezSan
+
+.REQUIREMENTS
+
+#>
+[CmdletBinding()]
+Param(
+[Parameter(Mandatory=$true, Position=0)][string[]]$DdcServers,
+[Parameter(Mandatory=$true, Position=1)][pscredential]$Credential
+)
+
+$DeliveryGroups = @()
+
+Foreach($ddc in $DdcServers){
+
+    $s01 = New-PsSession -Computer $ddc -Credential $Credential
+
+    try{
+        
+        $returnedData = Invoke-command -Session $s01 -ScriptBlock{
+
+            $array=@()
+
+            Add-PSSnapin citrix.*
+            $dg = (Get-BrokerDesktopGroup -AdminAddress localhost).Name
+
+            foreach ($x in $dg){
+
+                $hash = @{}
+                
+                $machines = Get-BrokerMachine -DesktopGroupName $x -MaxRecordCount 100000
+                $maintenance = $machines | ? InMaintenanceMode -eq $true
+                $availables = $machines | ? SummaryState -eq 'Available'
+                $inUse = $machines | Where-Object {$_.SummaryState -eq 'InUse' -OR $_.SummaryState -eq 'Disconnected' }
+                $unregistered = $machines | ? RegistrationState -eq 'Unregistered'
+                $off = $machines | ? PowerState -eq 'Off'
+                $percentageAvailable = [math]::Round($availables.count / $machines.count * 100,1)
+
+                $Object = New-Object PSObject
+                $Statistics = New-Object PSObject
+
+                $Statistics | add-member Noteproperty InUse $inUse.count
+                $Statistics | add-member Noteproperty Available $availables.count
+                $Statistics | add-member Noteproperty InMaintenance $maintenance.count
+                $Statistics | add-member Noteproperty Off $off.count
+                $Statistics | add-member Noteproperty Unregistered $unregistered.count
+                $Statistics | add-member Noteproperty PercentageAvailable $PercentageAvailable
+                $Statistics | add-member Noteproperty Total $machines.count
+
+                $Object | add-member Noteproperty DeliveryGroup $x
+                $Object | add-member Noteproperty Statistics $Statistics                
+
+                $array += $Object
+                
+                $hash = $null
+
+            }
+
+            return $array
+
+        } | Select -Property DeliveryGroup,Statistics
+
+    }catch{
+        Remove-PSSession $s01 -Confirm:$false
+    }
+
+    Remove-PSSession $s01 -Confirm:$false
+
+    $DeliveryGroups += $returnedData
+
+}
+
+ConvertTo-Json $DeliveryGroups -Depth 2 #-Compress
